3d1fdc2ef6b39daee59608514f7b3b971624ab47
hantungy <herbert.h.yee@gmail.com>
2016-10-19 16:31:20 -0400

    Made handles bars template to display user tasks

--- assets/scripts/bucketList/ui.js
+++ assets/scripts/bucketList/ui.js
@@ -1,22 +1,26 @@
 'use strict';
 
+const showTasksTemplate = require('../templates/task-list.handlebars');
+
 const success = () => console.log("success");
 
 const failure = () => console.log("failure");
 
 const getTaskSuccess = (data) => {
-	
-	let tasks = data.tasks;
 
-	$('.task-list').html('');
+	// let tasks = data.tasks;
+	//
+	// $('.task-list').html('');
+	//
+	// tasks.forEach((task) => {
+	// 	if (task.completed) {
+	// 	$('.task-list').append( '<li>' + task.title + ' | ' + task.description + ' | ' + 'completed' + '<button id="delete" -data-id=' + task._id + '> delete</button>' + '</li>' );
+	// 	} else {
+	// 		$('.task-list').append( '<li>' + task.title + ' | ' + task.description + ' ' + '<button id="delete" -data-id=' + task._id + '> delete</button> <button id="update" -data-id=' + task._id + '>update</button>' + '</li>' );
+	// 	}
+	// });
 
-	tasks.forEach((task) => {
-		if (task.completed) {
-		$('.task-list').append( '<li>' + task.title + ' | ' + task.description + ' | ' + 'completed' + '<button id="delete" -data-id=' + task._id + '> delete</button>' + '</li>' );
-		} else {
-			$('.task-list').append( '<li>' + task.title + ' | ' + task.description + ' ' + '<button id="delete" -data-id=' + task._id + '> delete</button> <button id="update" -data-id=' + task._id + '>update</button>' + '</li>' );
-		}
-	});
+	$('.task-list').html(showTasksTemplate(data));
 };
 
 module.exports = {
