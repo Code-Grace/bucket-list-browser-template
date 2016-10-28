cfcd5287124caf6bca136a6b099747c0501bc86f
hantungy <herbert.h.yee@gmail.com>
2016-10-28 16:11:07 -0400

    klfjaskfjkaljka

--- bundle.js
+++ bundle.js
@@ -443,7 +443,7 @@ webpackJsonp([0],[
 	var showTaskSuccess = function showTaskSuccess(data) {
 		var task = data.tasks;
 		$('.show').html('');
-		$('.show').append('<li style="color: white;">' + task.title + ' | ' + task.description + ' | ' + 'completed' + '<button style="color: black;" id="delete" -data-id=' + task._id + '> delete</button> | ' + task._id + '</li>');
+		$('.show').append('<li style="color: white;">' + task.title + ' | ' + task.description + ' | ' + task._id + '</li>');
 	};
 
 	module.exports = {
